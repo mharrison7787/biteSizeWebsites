2f65e30480628a15f99b49caf391d18d9528eaee
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-05-29 11:25:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+        {
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: "vmSocks-green-onWhite.jpg"
+        },
+        {
+            variantId: 2235,
+            variantColor: "blue",
+            variantImage: "vmSocks-blue-onWhite.jpg"
+        }
+    ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        subToCart() {
+            this.cart -= 1
+        }
+    }
+})  
