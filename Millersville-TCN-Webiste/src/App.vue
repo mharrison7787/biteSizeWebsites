f71f48f17534d7bfff5b00cb8d66d2fbe9100eba
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-07-15 16:06:50 -0400

    All current assests

---
+++ Millersville-TCN-Webiste/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <TCNnavbar></TCNnavbar>
+    <HelloWorld />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <slideDeck></slideDeck>
+        </div>
+        <div class="col-md-7">
+          <slideDeckRight></slideDeckRight>
+        </div>
+      </div>
+    </div>
+    <TCNBottmNavbar></TCNBottmNavbar>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import TCNnavbar from './components/TCNnavbar.vue';
+import slideDeck from './components/slideDeck.vue';
+import slideDeckRight from './components/slideDeckRight.vue';
+import TCNBottmNavbar from './components/TCNBottmNavbar.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TCNnavbar,
+    slideDeck,
+    slideDeckRight,
+    TCNBottmNavbar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ Millersville-TCN-Webiste/src/components/TCNnavbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <ul class="navbar-list">
+        <li class="navbar-item">HOME</li>
+        <li class="navbar-item">MEETINGS</li>
+        <li class="navbar-item"></li>
+        <li class="navbar-item">
+          <img src="../assets/MU_TCN-logo-min-p-500.png" width="250%">
+        </li>
+        <li class="navbar-item"></li>
+        <li class="navbar-item">JOB FAIR</li>
+        <li class="navbar-item">CONTACT</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.navbar {
+  background-color: black;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 23px 0;
+
+}
+
+.navbar-list {
+  display: flex;
+  list-style: none;
+  padding: 30;
+  margin: 0;
+  justify-content: center;
+  padding-left: 420px;
+}
+
+.navbar-item {
+  padding: 25px 35px;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
+  font-weight: bold;
+  text-transform: capitalize;
+  letter-spacing: 1.7px;
+  position: relative;
+  /* Added */
+
+}
+
+
+.navbar-item img {
+  position: absolute;
+  /* Added */
+  top: 120%;
+  /* Added */
+  left: 50%;
+  /* Added */
+  transform: translate(-50%, -50%);
+  /* Added */
+  z-index: 1;
+  /* Added z-index property */
+
+}
+
+.navbar::after {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 3px;
+  background-color: #ecb431;
+
+}
+</style>
